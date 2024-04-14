e9957d9953e00b971802048f1523ecca12acac02
chobkyu <qudqud97@naver.com>
2024-04-14 23:05:54 +0900

    update

--- src/util/getMonth.ts
+++ src/util/getMonth.ts
@@ -3,24 +3,24 @@ export const getMonth = (month:number) => {
 
     const year = date.getFullYear();//윤년 판단 용.
 
-    let gteDay;
+    let lteDay;
 
     if(month<8){ //8월 이전
         month != 2 //2월인지 아닌지 확인
             ? month%2 == 0 //2월이 아니면 이대로
-                ? gteDay = 30
-                : gteDay = 31
+                ? lteDay = 30
+                : lteDay = 31
             : year%4 == 0 //2월일 경우 윤년 확인 
-                ? gteDay = 29
-                : gteDay = 28
+                ? lteDay = 29
+                : lteDay = 28
     }else{ //8월 부터
-        month%2 == 0 ? gteDay = 31 : gteDay = 30
+        month%2 == 0 ? lteDay = 31 : lteDay = 30
     }
 
     let monthStr = month<10 ? `0${month}` : month.toString();
     const dayObj = {
-        lte : `${year}-${monthStr}-01`,
-        gte : `${year}-${monthStr}-${gteDay}`
+        gte : `${year}-${monthStr}-01`,
+        lte : `${year}-${monthStr}-${lteDay}`
     };
 
     return dayObj;
