03056b3ce5c0170763707b154fccb638e0da4fde
cxzaqq <roy1228@naver.com>
2024-04-18 00:07:22 +0900

    다시 Choice로

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -2,7 +2,7 @@ import { HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma.service';
 import { InsertQuestionDto } from './Dto/question.dto';
-import { question_choice, Visit } from '@prisma/client';
+import { Choice, Visit } from '@prisma/client';
 import { UserService } from 'src/user/user.service';
 import { PermitListDto } from './Dto/permitUser.dto';
 
@@ -57,7 +57,7 @@ export class AdminService {
                     data:{
                         question:insertQuestionDto.question,
                         type:Visit[insertQuestionDto.type],
-                        choice:question_choice[insertQuestionDto.choice],
+                        choice:Choice[insertQuestionDto.choice],
                         note:insertQuestionDto.note,
                     }
                 });
