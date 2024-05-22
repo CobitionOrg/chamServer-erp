d028780b5bfbd868ea464613aef10152db2bcfc2
chobkyu <98572357+chobkyu@users.noreply.github.com>
2024-05-22 16:52:10 +0900

    update

---
+++ src/util/getSendTitle.ts
@@ -0,0 +1,32 @@
+export const getSendTitle = () =>{
+    const targetDays = ['Monday', 'Tuesday', 'Thursday', 'Friday'];
+    const today = new Date();
+    const todayDay = today.getDay();
+
+    const targetIndices = targetDays.map(day => {
+        switch(day.toLowerCase()){
+            case 'monday' : return 1;
+            case 'tuesday' : return 2;
+            case 'thursday' : return 4;
+            case 'friday' : return 5;
+            default: return null
+        }
+    }).filter(i => i !== null);
+
+    let colosetDate = null;
+    let minDaysDiff = Infinity;
+
+    targetIndices.forEach(targetIdx => {
+        let daysDiff = (targetIdx - todayDay +7) % 7;
+        if(daysDiff === 0){
+            daysDiff = 7;
+        }
+
+        if(daysDiff < minDaysDiff){
+            minDaysDiff = daysDiff;
+            colosetDate = new Date(today.getTime() + daysDiff *24*60*60*1000);
+        }
+    });
+
+    return colosetDate;
+}
