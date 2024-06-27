cdd62d2e237871fee58c420010fee4a7b7ab1233
jeon <youngman0515@naver.com>
2024-06-27 18:29:17 +0900

    feat:로그처리완료

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { TasksRepository } from './tasks.repository';
+import { LogService } from 'src/log/log.service';
 
 @Injectable()
 export class TasksService {
     constructor(
         private readonly tasksRepository: TasksRepository,
+        private readonly logService : LogService,
     ){}
 
     private readonly logger = new Logger(TasksService.name);
@@ -13,6 +15,11 @@ export class TasksService {
     @Cron('0 59 23 * * *')
     async handleCron(){
         this.logger.debug('delete s3 data');
+        await this.logService.createLog(
+            `delete s3 data`,
+            'delete s3 data',
+            null
+        );
         await this.tasksRepository.deleteS3Data();
     }
 }
