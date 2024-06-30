1dd6d3406e74ad83d35b862069a196f0dd0d2d0b
chobkyu <qudqud97@naver.com>
2024-07-01 01:00:23 +0900

    add visit list api

---
+++ src/visit/visit.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { VisitController } from './visit.controller';
+import { VisitService } from './visit.service';
+import { LogService } from 'src/log/log.service';
+import { LogRepository } from 'src/log/log.repository';
+import { PrismaService } from 'src/prisma.service';
+import { VisitRepository } from './visit.repository';
+
+@Module({
+    controllers:[VisitController],
+    providers: [
+        VisitService,
+        Crypto,
+        LogService,
+        LogRepository,
+        PrismaService,
+        VisitRepository
+    ]
+})
+export class VisitModule {}
---
+++ src/visit/visit.repository.ts
@@ -0,0 +1,85 @@
+import { HttpStatus, Injectable, Logger } from "@nestjs/common";
+import { PrismaService } from "src/prisma.service";
+
+@Injectable()
+export class VisitRepository {
+    constructor(
+        private prisma: PrismaService,
+    ){}
+
+    private readonly logger = new Logger(VisitRepository.name);
+
+    async visitOrder(id: number) {
+        try{
+            await this.prisma.order.update({
+                where:{id:id},
+                data:{orderSortNum:-1}
+            });
+
+            return {success:true, status:HttpStatus.OK};
+        }catch(err){
+            this.logger.error(err);
+            return {
+                success: false,
+                status : HttpStatus.INTERNAL_SERVER_ERROR
+            }
+
+        }
+    }
+
+    async visitList(orderConditions, patientConditions){
+        try{
+            const list = await this.prisma.order.findMany({
+                where:{
+                    orderSortNum:-1,
+                    isComplete: false,
+                    ...orderConditions,
+                    ...patientConditions
+                },
+                select:{
+                    id: true,
+                    route: true,
+                    message: true,
+                    cachReceipt: true,
+                    typeCheck: true,
+                    consultingTime: true,
+                    payType: true,
+                    outage: true,
+                    consultingType: true,
+                    phoneConsulting: true,
+                    isFirst: true,
+                    date: true,
+                    orderSortNum: true,
+                    remark: true,
+                    addr:true,
+                    isPickup: true,
+                    patient: {
+                        select: {
+                            id: true,
+                            name: true,
+                            addr: true,
+                            phoneNum: true,
+                        }
+                    },
+                    orderItems: {
+                        select: {
+                            item: true,
+                            type: true,
+                        }
+                    },
+
+                }
+            });
+
+            return {success:true, list, status:HttpStatus.OK};
+
+        }catch(err){
+            this.logger.error(err);
+            return {
+                success: false,
+                status : HttpStatus.INTERNAL_SERVER_ERROR
+            }
+
+        }
+    }
+}
