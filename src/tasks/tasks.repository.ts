955944314192dda554b68b8c2258acc08d5ae222
chobkyu <98572357+chobkyu@users.noreply.github.com>
2024-07-05 12:18:01 +0900

    update

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,4 +1,6 @@
 import { HttpException, HttpStatus, Injectable, Logger } from "@nestjs/common";
+import { CancelOrderDto } from "src/erp/Dto/cancelOrder.dto";
+import { ErpService } from "src/erp/erp.service";
 import { PrismaService } from "src/prisma.service";
 import { deleteUploadObject } from "src/util/s3";
 const fs = require('fs');
@@ -7,7 +9,7 @@ const path = require('path');
 @Injectable()
 export class TasksRepository {
     constructor(
-        private prisma: PrismaService
+        private prisma: PrismaService,
     ) { }
 
     private readonly logger = new Logger(TasksRepository.name);
@@ -62,6 +64,93 @@ export class TasksRepository {
                 });
             });
         } catch (err) {
+            this.logger.error(err);
+            throw new HttpException({
+                success: false,
+                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                msg: '내부서버 에러'
+            },
+                HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    
+    async deleteNotCallOrder() {
+        try{
+            const today = new Date();
+
+            const twoMonthAgo = new Date(today.setMonth(today.getMonth() - 2));
+
+            const oldRecords = await this.prisma.order.findMany({
+                where:{
+                    date:{
+                        lt: twoMonthAgo
+                    },
+                },
+                select:{
+                    id: true,
+                    patient:{select:{id:true}},
+                    isFirst:true
+                }
+            });
+
+            for(const e of oldRecords) {
+                const cancelOrderDto: CancelOrderDto = {
+                    orderId: e.id,
+                    patientId: 0,
+                    isFirst: false
+                }
+
+                if (cancelOrderDto.isFirst) {
+                    //초진 일 시 환자 데이터까지 soft delete
+                    const orderId = cancelOrderDto.orderId;
+                    const patientId = cancelOrderDto.patientId;
+    
+                    await this.prisma.$transaction(async (tx) => {
+                        //orderBodyType soft delete
+                        await tx.orderBodyType.update({
+                            where: { orderId: orderId },
+                            data: { useFlag: false }
+                        });
+    
+                        //orderItem soft delete
+                        await tx.orderItem.updateMany({
+                            where: { orderId: orderId },
+                            data: { useFlag: false }
+                        });
+    
+                        //order soft delete
+                        await tx.order.update({
+                            where: { id: orderId },
+                            data: { useFlag: false }
+                        });
+    
+                        //patient soft delete
+                        await tx.patient.update({
+                            where: { id: patientId },
+                            data: { useFlag: false } 
+                        });
+                    });
+    
+                    return { success: true, status: HttpStatus.OK, msg: '초진 삭제' }
+                } else {
+                    //재진 일 시 환자 데이터는 가지고 있어야 되기 때문에 오더 정보만 삭제
+                    const orderId = cancelOrderDto.orderId;
+    
+                    //오더만 useFlag false로 변경
+                    await this.prisma.order.update({
+                        where: { id: orderId },
+                        data: { useFlag: false }
+                    });
+    
+                    return { success: true, status: HttpStatus.OK, msg: '재진 삭제' }
+    
+                }
+
+
+            }
+        }catch(err){
             this.logger.error(err);
             throw new HttpException({
                 success: false,
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -33,6 +33,17 @@ export class TasksService {
         );
         await this.tasksRepository.deleteSaveFile();
     }
+
+    @Cron('0 59 23 * * * ')
+    async deleteNotCallOrder(){
+        this.logger.debug('delete save file');
+        await this.logService.createLog(
+            `상담 미연결 오더 삭제`,
+            '상담 미연결 오더 삭제',
+            null
+        );
+        await this.tasksRepository.deleteNotCallOrder();
+    }
     
     //매주 토요일 오전 9시 후기 안내 알람톡 발송
     @Cron('0 9 * * 6')
