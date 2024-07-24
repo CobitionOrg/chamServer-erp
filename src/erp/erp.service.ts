ae2a69343b5e7db7cebc84811ca06cb0a63cadf6
cxzaqq <roy1228@naver.com>
2024-07-24 19:02:02 +0900

    주석 처리

--- src/erp/erp.service.ts
+++ src/erp/erp.service.ts
@@ -2176,57 +2176,57 @@ export class ErpService {
                 const encryptedAddr = this.crypto.encrypt(combineOrderDto.addr);
 
                 for(let i = 0; i<orders.length; i++){
-                    const check = await this.isInTempOrder(orders[i].id);
-                    if(check) {
-                    // 발송 목록에 있는 항목이면 update
-                        const tempOrder = await tx.tempOrder.updateMany({
-                            where: {
-                                orderId: orders[i].id
-                            },
-                            data: {
-                                orderSortNum: 6,
-                            }
-                        });
+                    // const check = await this.isInTempOrder(orders[i].id);
+                    // if(check) {
+                    // // 발송 목록에 있는 항목이면 update
+                    //     const tempOrder = await tx.tempOrder.updateMany({
+                    //         where: {
+                    //             orderId: orders[i].id
+                    //         },
+                    //         data: {
+                    //             orderSortNum: 6,
+                    //         }
+                    //     });
 
-                        const order = await tx.order.update({
-                            where: {
-                                id: orders[i].id
-                            },
-                            data: {
-                                combineNum: newCombineNum
-                            }
-                        });
-                    } else {
-                    // 아니면 create
-                    const res = await tx.tempOrder.create({
-                        data: {
-                            route: orders[i].route,
-                            message: orders[i].message,
-                            cachReceipt: orders[i].cachReceipt,
-                            typeCheck: orders[i].typeCheck,
-                            consultingTime: orders[i].consultingTime,
-                            payType: orders[i].payType,
-                            essentialCheck: orders[i].essentialCheck,
-                            outage: orders[i].outage,
-                            consultingType: orders[i].consultingType,
-                            phoneConsulting: orders[i].phoneConsulting,
-                            isComplete: orders[i].isComplete,
-                            isFirst: orders[i].isFirst,
-                            date: orders[i].date,
-                            orderSortNum: 6,
-                            addr: encryptedAddr,
-                            order: {
-                                connect: { id: orders[i].id }
-                            },
-                            patient: {
-                                connect: { id: orders[i].patientId }
-                            },
-                            sendList: {
-                                connect: { id: sendListId }
-                            }
-                        }
-                    });
-                    }
+                    //     const order = await tx.order.update({
+                    //         where: {
+                    //             id: orders[i].id
+                    //         },
+                    //         data: {
+                    //             combineNum: newCombineNum
+                    //         }
+                    //     });
+                    // } else {
+                    // // 아니면 create
+                    // const res = await tx.tempOrder.create({
+                    //     data: {
+                    //         route: orders[i].route,
+                    //         message: orders[i].message,
+                    //         cachReceipt: orders[i].cachReceipt,
+                    //         typeCheck: orders[i].typeCheck,
+                    //         consultingTime: orders[i].consultingTime,
+                    //         payType: orders[i].payType,
+                    //         essentialCheck: orders[i].essentialCheck,
+                    //         outage: orders[i].outage,
+                    //         consultingType: orders[i].consultingType,
+                    //         phoneConsulting: orders[i].phoneConsulting,
+                    //         isComplete: orders[i].isComplete,
+                    //         isFirst: orders[i].isFirst,
+                    //         date: orders[i].date,
+                    //         orderSortNum: 6,
+                    //         addr: encryptedAddr,
+                    //         order: {
+                    //             connect: { id: orders[i].id }
+                    //         },
+                    //         patient: {
+                    //             connect: { id: orders[i].patientId }
+                    //         },
+                    //         sendList: {
+                    //             connect: { id: sendListId }
+                    //         }
+                    //     }
+                    // });
+                    // }
                 }
                 // await tx.tempOrder.updateMany({
                 //     where: {
@@ -2274,30 +2274,30 @@ export class ErpService {
         }
     }
 
-    /**
-     * 합배송 시 발송 목록에 있는 항목인지 체크
-     */
-    async isInTempOrder(orderId: number) {
-        try {
-            const res = await this.prisma.tempOrder.findFirst({
-                where: {
-                    orderId: orderId
-                }
-            });
+    // /**
+    //  * 합배송 시 발송 목록에 있는 항목인지 체크
+    //  */
+    // async isInTempOrder(orderId: number) {
+    //     try {
+    //         const res = await this.prisma.tempOrder.findFirst({
+    //             where: {
+    //                 orderId: orderId
+    //             }
+    //         });
 
-            console.log(res);
+    //         console.log(res);
 
-            return res === null ? false : true;
-        } catch (err) {
-            this.logger.error(err);
-            throw new HttpException({
-                success: false,
-                status: HttpStatus.INTERNAL_SERVER_ERROR
-            },
-                HttpStatus.INTERNAL_SERVER_ERROR
-            );
-        }
-    }
+    //         return res === null ? false : true;
+    //     } catch (err) {
+    //         this.logger.error(err);
+    //         throw new HttpException({
+    //             success: false,
+    //             status: HttpStatus.INTERNAL_SERVER_ERROR
+    //         },
+    //             HttpStatus.INTERNAL_SERVER_ERROR
+    //         );
+    //     }
+    // }
 
     /**
      * 분리발송 작업
