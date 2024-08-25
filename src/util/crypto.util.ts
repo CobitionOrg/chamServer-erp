e71749aa116277e4d5d94e2a3cfc1549db0c30d5
cxzaqq <roy1228@naver.com>
2024-08-26 00:03:59 +0900

    암복호화 try catch 추가

--- src/util/crypto.util.ts
+++ src/util/crypto.util.ts
@@ -15,20 +15,33 @@ export class Crypto {
 
     this.key = Buffer.from(keyBase64, 'base64');
     this.iv = Buffer.from(ivBase64, 'base64');
-    
   }
 
   encrypt(text: string): string {
-    const cipher = crypto.createCipheriv(this.algorithm, this.key, this.iv);
-    let encrypted = cipher.update(text, 'utf8', 'hex');
-    encrypted += cipher.final('hex');
-    return encrypted;
+    try {
+      const cipher = crypto.createCipheriv(this.algorithm, this.key, this.iv);
+      let encrypted = cipher.update(text, 'utf8', 'hex');
+      encrypted += cipher.final('hex');
+      return encrypted;
+    } catch (error) {
+      console.error('Encryption error:', error);
+      return text;
+    }
   }
 
   decrypt(encryptedText: string): string {
-    const decipher = crypto.createDecipheriv(this.algorithm, this.key, this.iv);
-    let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
-    decrypted += decipher.final('utf8');
-    return decrypted;
+    try {
+      const decipher = crypto.createDecipheriv(
+        this.algorithm,
+        this.key,
+        this.iv,
+      );
+      let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
+      decrypted += decipher.final('utf8');
+      return decrypted;
+    } catch (error) {
+      console.error('Decryption error:', error);
+      return encryptedText;
+    }
   }
 }
