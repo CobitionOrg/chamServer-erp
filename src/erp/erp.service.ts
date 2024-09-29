c2cf5f0e2c94fbb220ad1f72bbc6367298b5d1d4
cxzaqq <roy1228@naver.com>
2024-09-30 04:22:20 +0900

    [issue] 고객이 주문 수정 시 환자 테이블에만 적용되는 현상 #395

--- src/erp/erp.service.ts
+++ src/erp/erp.service.ts
@@ -1220,7 +1220,8 @@ export class ErpService {
                     },
                     data: {
                         payType: objOrder.payType,
-                        price: price
+                        price: price,
+                        addr: encryptedAddr,
                     }
                 });
 
